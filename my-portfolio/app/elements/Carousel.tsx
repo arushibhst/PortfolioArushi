d6992169d96a8f40360a34bbacd25b46f6242d16
arushibhst <arushikbh@gmail.com>
2025-11-30 19:06:38 +1300

    Created the homepage of my website. The homepage has a banner, and carousel of images

---
+++ my-portfolio/app/elements/Carousel.tsx
@@ -0,0 +1,32 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export function CarouselComponent(){
+    return (
+        <div className="w-full flex justify-center object-center mt-10">
+            <Carousel className="w-full max-w-xl">
+                <CarouselContent>
+                    <CarouselItem className="flex justify-center">
+                        <img src="/recapp.png" alt="Logo of recapp app" className="rounded-lg w-169 h-125" />
+                    </CarouselItem>
+                    <CarouselItem className="flex justify-center">
+                        <img src="/mountain.png" alt="Mountain Hikers video game" className="object-cover rounded-lg w-169 h-125" />
+                    </CarouselItem>
+                    <CarouselItem className="flex justify-center">
+                        <img src="/avc.jpg" alt="Autonomous Vehicle following obstacle course" className="object-cover rounded-lg w-169 h-125" />
+                    </CarouselItem>
+                    <CarouselItem className="flex justify-center">
+                        <img src="/penguin.png" alt="Logo of Project Kororā" className="object-cover rounded-lg w-169 h-125" />
+                    </CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious className="absolute bg-indigo-700" />     
+                <CarouselNext className="absolute bg-indigo-700" />
+            </Carousel>
+            </div>
+    );
+}
